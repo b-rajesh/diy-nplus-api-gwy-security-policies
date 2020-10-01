91684a31076c5df6b93d5d0f53a434612b1debb6
Rajesh Bavananatham <rajesh.bavan@gmail.com>
2020-10-01 22:48:17 +1000

    Fixed NAP module installation with NGINX R22 with diy config

--- install_nginx.sh
+++ install_nginx.sh
@@ -7,24 +7,32 @@ echo "Copying nginx keys to /etc/ssl/ ...."
 sudo mv /tmp/etc/ssl/nginx /etc/ssl/
 sudo chown  root:root -R /etc/ssl/nginx/
 sudo chmod  777 -R /etc/ssl/nginx/
-sudo wget https://nginx.org/keys/nginx_signing.key
-sudo apt-key add nginx_signing.key
-sudo apt-get -y install apt-transport-https lsb-release ca-certificates wget
+
+sudo apt-get install apt-transport-https lsb-release ca-certificates wget
+sudo wget https://cs.nginx.com/static/keys/nginx_signing.key && sudo apt-key add nginx_signing.key
 printf "deb https://plus-pkgs.nginx.com/ubuntu `lsb_release -cs` nginx-plus\n" | sudo tee /etc/apt/sources.list.d/nginx-plus.list
-sudo wget -q -O /etc/apt/apt.conf.d/90nginx https://cs.nginx.com/static/files/90nginx
+sudo wget -P /etc/apt/apt.conf.d https://cs.nginx.com/static/files/90nginx
+
+echo "Installing nginx plus "
 sudo apt-get -y update
 sudo apt-get install -y nginx-plus
+
+echo "Installing njs, opentracing & headers-more dynamic modules..."
 sudo apt-get install -y nginx-plus-module-njs
 sudo apt-get install -y nginx-plus-module-opentracing
 sudo apt-get install -y nginx-plus-module-headers-more
+
+sudo mv -f /tmp/etc/nginx /etc/
+echo "Installing NGINX App Protect dynamic modules...."
 sudo apt-get install -y app-protect
 
-echo "Making nginx backup ...."
-sudo mv /etc/nginx /tmp/etc/nginx-backup
-sudo mv /tmp/etc/nginx /etc/
+echo "Linking dynamic module and changing the ownership..."
 sudo ln -s /usr/lib/nginx/modules /etc/nginx/modules
 sudo chown  root:root -R /etc/nginx/
 sudo chmod  777 -R /etc/nginx/
+
+echo "Verifying nginx config...."
 sudo nginx -t -c /etc/nginx/nginx.conf
+
 echo "Restarting nginx ...."
-#sudo service nginx restart
+sudo service nginx restart
